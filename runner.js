5a8dc125cbf5e65630631d238698db0d460dd80c
Sammy <samantha.megan10@gmail.com>
2021-03-31 23:19:42 -0600

    cards assigned, need to rreturn hand

--- runner.js
+++ runner.js
@@ -1,7 +1,8 @@
-const { forEach } = require('./cards')
 let cards = require('./cards')
 let table = []
 let players = []
+
+// console.log(cards)
 
 // Get the logged in players from database
 let loggedIn = ['Sammy', 'Ollie', 'Ben']
@@ -17,36 +18,68 @@ loggedIn.forEach((l, i) => {
   }
 })
 
-// console.log(players)
+// Shuffle the cards 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  // console.log("Current Index is" + currentIndex)
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+shuffle(cards)
+
+// Number of cards all the players get
+let cardCount = [...cards]
+// console.log(cardCount)
+let numCards = ((cardCount.length) - (cardCount.length % players.length)) / players.length
+console.log("Num cards is " + numCards)
 
 // Assign cards randomly to each of the players
-let numCards = ((cards.length) - (cards.length % players.length)) / players.length
-
-
 const deal = (player) => {
-  for (i = 0; i < numCards; i++) {
-    let random = Math.floor(Math.random() * cards.length)
-    console.log(random)
-    if (!cards[random]) {
-      console.log("stuck")
 
-    } else {
-      player.cards.push(cards[random])
-      cards.splice(random)
-      console.log("removing")
-    }
+  let i = 0
+
+  while (i < numCards) {
+    player.cards.push(cards[0])
+    cards.shift()
+    i++
   }
 }
 
-players.forEach((player, i) => {
-  deal(player)
-  console.log(player)
+let dealing = new Promise(function (resolve, reject) {
+  players.forEach((player, i) => {
+    deal(player)
+
+    resolve(cards);
+    reject("Failed")
+  })
 })
 
-// console.log(numCards)
-// console.log(cards.length)
-// console.log(cards.length % players.length)
-// console.log(random)
-// console.log(cards)
+dealing.then(
+  function (cards) {
+    if (cards.length > 0) {
+      console.log("more than 1")
+      cards.forEach((card, i) => players[i].cards.push(card))
+    }
+    console.log(players)
+  }
+);
+
+console.log(players)
+
+
 
 // Each player picks a card and puts on the table 
